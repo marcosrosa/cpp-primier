995a2fc6b09e8feceace1f6ff67dcfa81fdce6b0
Marcos Rosa <marcosrosa@jfes.jus.br>
2015-01-23 13:48:42 -0300

    testes com classes sequencial conteiners

--- src/SalesData.h
+++ src/SalesData.h
@@ -14,9 +14,9 @@
 
 class SalesData {
 	// nao usuaria funcoes friends
-	friend SalesData     add(const SalesData&, const SalesData&);
-	friend std::istream& read(std::istream&, SalesData&);
-	friend std::ostream& print(std::ostream&, const SalesData&);
+	friend SalesData      add(const SalesData &, const SalesData &);
+	friend std::istream & read(std::istream &, SalesData &);
+	friend std::ostream & print(std::ostream &, const SalesData &);
 	public:
 		// construtores
 		SalesData() = default;
@@ -29,24 +29,28 @@ class SalesData {
 
 		// metodos publicos
 		std::string getBookNo()    const { return bookNo; }
+
 		unsigned    getUnitsSold() const { return unitsSold; }
+		void        setUnitsSold(unsigned x) { x > 0 ? unitsSold = x : unitsSold=1; }
+
 		double      getRevenue()   const { return revenue; }
-		SalesData&  combine(const SalesData &);
+		void        setRevenue(double rv) { revenue = rv; }
+
+		SalesData & combine(const SalesData &);
 		double      getAvgPrice()  const { return avgPrice(); }
 	private:
 		// dados da classe
 		std::string bookNo;
-		unsigned unitsSold = 0;
+		unsigned unitsSold = 1;
 		double revenue = 0.0;
 
 		// funcao privada
 		double avgPrice() const { return unitsSold ? revenue/unitsSold : 0; }
 };
 
-SalesData add(const SalesData&, const SalesData&);
-std::istream &read(std::istream&, SalesData&);
-std::ostream &print(std::ostream&, const SalesData&);
-
-
+/* declarations of friends function for others to user */
+SalesData      add(const SalesData &, const SalesData &);
+std::istream & read(std::istream &, SalesData &);
+std::ostream & print(std::ostream &, const SalesData &);
 
 #endif /* SALESDATA_H_ */
--- src/trem.cpp
+++ src/trem.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <assert.h>
 
 #include "trem.h"
 
@@ -14,6 +15,7 @@ int trem::cnt2 = 30;
 constexpr int trem::sequencia[];
 
 int trem::arrayTest() {
+	std::cout << "class-> trem.cpp method -> arrayTest()" << std::endl;
 	int ia[] = {8,7,6};
 	for (int x = 0; x < 3; ++x) {
 		std::cout << "then num:"<< ia[x] << std::endl;
@@ -24,10 +26,13 @@ int trem::arrayTest() {
 	for (int k = 0; k < 5; ++k)
 		std::cout << "item array: " << sequencia[k] << std::endl;
 
+	std::cout << "fim - arrayTest()" << std::endl;
 
 	return 0;
 }
 
 void trem::doTheTest() {
+	assert(std::cout << "class-> trem.cpp method -> doTheTest()" << std::endl);
 	arrayTest();
+	assert(std::cout << "fim -> trem.cpp method -> doTheTest()" << std::endl);
 }
